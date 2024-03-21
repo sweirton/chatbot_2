99c53c8bcd0df50d7f4c7db241f6c0893efb4dd6
Sam Weirton <109814114+sweirton@users.noreply.github.com>
2024-03-21 11:48:42 -0500

    Update history_interface.py
    Added in the ability to select a chat session. This sends out a signal to the main window.

--- history_interface.py
+++ history_interface.py
@@ -1,7 +1,10 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QListWidget
+from PySide6.QtCore import Signal
 import os
 
 class ChatHistoryWidget(QWidget):
+    sessionSelected = Signal(str)
+
     def __init__(self, parent=None, profile_name=''):
         super().__init__(parent)
 
@@ -20,6 +23,12 @@ class ChatHistoryWidget(QWidget):
         self.history_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'profiles', self.profile_name, 'chat_history')
         self.populateWithFilenames()
 
+         # Select a history item in the list
+        self.chat_history_list.setSelectionMode(QListWidget.SingleSelection)
+
+        # Connect the itemSelectionChanged signal to a slot
+        self.chat_history_list.itemSelectionChanged.connect(self.onSelectionChanged)
+
     def populateWithFilenames(self):
         # List all files in the specified directory
         session_files = os.listdir(self.history_dir)
@@ -33,3 +42,11 @@ class ChatHistoryWidget(QWidget):
         # Add each JSON filename to the chat history list widget
         for filename in json_files:
             self.chat_history_list.addItem(filename)
+
+    def onSelectionChanged(self):
+        if selected_items := self.chat_history_list.selectedItems():
+            selected_filename = selected_items[0].text()
+            # Construct the full path for the selected session
+            selected_file_path = os.path.join(self.history_dir, selected_filename)
+            # Emit the full path
+            self.sessionSelected.emit(selected_file_path)
